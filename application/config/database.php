5d41220a28fd15110b42519d4fe4f5947fb82e3b
dhanisetiaji <dhanisetiaji99@gmail.com>
2019-09-14 23:25:40 +0700

    confignya

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'db_oshop';
+$db['default']['hostname'] = 'localhost:3306';
+$db['default']['username'] = 'oshop';
+$db['default']['password'] = '^xG53y5i';
+$db['default']['database'] = 'dropspac_oshop';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
